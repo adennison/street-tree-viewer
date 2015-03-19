0c873a6d5f7ca8e1747fbe5074aff2e91b233643
Andrew Dennison <andrew@gaiaresources.com.au>
2015-03-19 12:26:46 +0800

    First commit

---
+++ static/data/get_photos.py
@@ -0,0 +1,9 @@
+import os,csv
+
+with open('/home/andrew/workspace/trees.csv', 'rb') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	for row in reader:
+		server = 'gaia@gaiabdrs.gaiaresources.com.au'
+		path = '/data/filestores/gaiabdrs/bdrs_core_filestore/au/com/gaiaresources/bdrs/model/taxa/AttributeValue/' + row[0] + '/' + row[1]
+		target = '/home/andrew/workspace/street-tree-viewer/data/images/' + row[1]
+		os.system('scp "%s:%s" "%s"' % (server, path, target))
